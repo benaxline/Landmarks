30dd243f06190f47b7698028aeb249432da06f44
Benjamin Axline <baxline@bu.edu>
2024-06-18 12:51:00 -0400

    Composing complex interfaces

---
+++ CategoryItem.swift
@@ -0,0 +1,18 @@
+//
+//  CategoryItem.swift
+//  Landmarks
+//
+//  Created by Benjamin Axline on 6/18/24.
+//
+
+import SwiftUI
+
+struct CategoryItem: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CategoryItem()
+}
---
+++ CategoryRow.swift
@@ -0,0 +1,18 @@
+//
+//  CategoryRow.swift
+//  Landmarks
+//
+//  Created by Benjamin Axline on 6/18/24.
+//
+
+import SwiftUI
+
+struct CategoryRow: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CategoryRow()
+}
